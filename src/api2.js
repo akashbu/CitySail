ff25ec59c948e736ebb85a24af1f8afa65d6c8eb
Akash Butala <akash.butala.365@gmail.com>
2023-10-09 19:01:58 -0700

    Added Shotest distance code on UI

--- src/api2.js
+++ src/api2.js
@@ -45,27 +45,28 @@ async function geocode(lat, lon) {
 
 
   const locations = [
-    { name: 'Fullerton', lat: 33.8704, lon: -117.9242 },
-    { name: 'Long Beach', lat: 33.770050, lon: -118.193741 },
-    { name: 'Brea', lat: 33.915279, lon: -117.888207 },
-    { name: 'Placentia', lat: 33.871075, lon: -117.862755 },
-    { name: 'Yorba Linda', lat: 33.888531, lon: -117.824310 },
-    { name: 'Bueuna Park', lat: 33.869110, lon: -117.993952 },
-    { name: 'Cerritos', lat: 33.862607, lon: -118.052657 },
-    { name: 'Anaheim', lat: 33.836594, lon: -117.914299},
-    { name: 'Norwalk', lat: 33.913780, lon: -118.070099 },
-    { name: 'Bellflower', lat: 33.881683, lon: -118.117012 },
-    { name: 'La Mirada', lat: 33.908989, lon: -118.009949 },
-    { name: 'Lakewood', lat: 33.853626, lon: -118.133957 },
-    { name: 'Los Alamitos', lat: 33.804970, lon: -118.071800 },
-    { name: 'Seal Beach', lat: 33.741409, lon: -118.104767 },
-    { name: 'La Habra', lat: 33.931858, lon: -117.946136 },
-    { name: 'Garden Grove', lat: 33.774269, lon: -117.937996 },
-    { name: 'Santa Ana', lat: 33.745472, lon: -117.867653 },
-    { name: 'Carson', lat: 33.827820, lon: -118.272346 },
-    { name: 'Chino Hills', lat: 33.991200, lon: -117.760861 },
-    { name: 'Diamond Bar', lat: 34.026490, lon: -117.810264 },
+    { id: 0, name: 'Fullerton', lat: 33.8704, lon: -117.9242 },
+    { id: 1, name: 'Long Beach', lat: 33.770050, lon: -118.193741 },
+    { id: 2, name: 'Brea', lat: 33.915279, lon: -117.888207 },
+    { id: 3, name: 'Placentia', lat: 33.871075, lon: -117.862755 },
+    { id: 4, name: 'Yorba Linda', lat: 33.888531, lon: -117.824310 },
+    { id: 5, name: 'Buena Park', lat: 33.869110, lon: -117.993952 },
+    // { id: 6, name: 'Cerritos', lat: 33.862607, lon: -118.052657 },
+    // { id: 7, name: 'Anaheim', lat: 33.836594, lon: -117.914299 },
+    // { id: 8, name: 'Norwalk', lat: 33.913780, lon: -118.070099 },
+    // { id: 9, name: 'Bellflower', lat: 33.881683, lon: -118.117012 },
+    // { id: 10, name: 'La Mirada', lat: 33.908989, lon: -118.009949 },
+    // { id: 11, name: 'Lakewood', lat: 33.853626, lon: -118.133957 },
+    // { id: 12, name: 'Los Alamitos', lat: 33.804970, lon: -118.071800 },
+    // { id: 13, name: 'Seal Beach', lat: 33.741409, lon: -118.104767 },
+    // { id: 14, name: 'La Habra', lat: 33.931858, lon: -117.946136 },
+    // { id: 15, name: 'Garden Grove', lat: 33.774269, lon: -117.937996 },
+    // { id: 16, name: 'Santa Ana', lat: 33.745472, lon: -117.867653 },
+    // { id: 17, name: 'Carson', lat: 33.827820, lon: -118.272346 },
+    // { id: 18, name: 'Chino Hills', lat: 33.991200, lon: -117.760861 },
+    // { id: 19, name: 'Diamond Bar', lat: 34.026490, lon: -117.810264 },
   ];
+  
   
   // Calculate distances between all pairs of locations and populate a distance matrix
   const numLocations = locations.length;
@@ -101,16 +102,20 @@ async function createDistanceMatrix (){
 }
 
 function createOpitimizeMatrix(){
-    distanceMatrix[1][3] = 999
+  const intermediates = {}; 
     for (let k = 0; k < numLocations; k++) {
       for (let i = 0; i < numLocations; i++) {
         for (let j = 0; j < numLocations; j++) {
           if (distanceMatrix[i][k] + distanceMatrix[k][j] < distanceMatrix[i][j]) {
             distanceMatrix[i][j] = distanceMatrix[i][k] + distanceMatrix[k][j];
+            const key = `${i} ${locations[i].name}-${j} ${locations[j].name}`;
+            intermediates[key] = locations[k].name; 
           }
         }
       }
     }
+    console.log("Intermediate Nodes")
+    console.log(intermediates)
   }
 
     // Function to print the distanceMatrix in matrix format
@@ -136,9 +141,10 @@ function printDistanceMatrix(matrix) {
 
 async function main(){
 await createDistanceMatrix()
-saveDistanceMatrixToFile(distanceMatrix, 'distance_matrix2.json');
+// saveDistanceMatrixToFile(distanceMatrix, 'distance_matrix2.json');
 printDistanceMatrix(distanceMatrix);
 createOpitimizeMatrix()
+console.log("Optimized Distance Matrix:")
 printDistanceMatrix(distanceMatrix);
 }
 
